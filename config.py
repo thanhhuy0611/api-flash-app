4c8ef4fc6b27728dbb8ad2f6684d03eeedb8aad4
= <=>
2019-12-05 10:32:32 +0700

    config data url

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 class Config(object):
   SECRET_KEY = os.environ.get("FLASK_SECRET_KEY") or "supersekrit"
-  SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_POSTGRES')
+  SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_POSTGRES') or os.environ.get('DATABASE_URL')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   FACEBOOK_OAUTH_CLIENT_ID = os.environ.get("FACEBOOK_OAUTH_CLIENT_ID")
   FACEBOOK_OAUTH_CLIENT_SECRET = os.environ.get("FACEBOOK_OAUTH_CLIENT_SECRET")
